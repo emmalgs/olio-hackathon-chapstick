5cddafcd7403cd1cc8bd262ece370a6a999cbe39
Emma Gerigscott <elgerig@gmail.com>
2023-08-17 11:19:48 -0700

    connect to chat api
    Co-authored-by: Sarah Reimann <reimannsarah@users.noreply.github.com>

--- src/components/ChadController.jsx
+++ src/components/ChadController.jsx
@@ -13,13 +13,31 @@ function ChadController() {
 
   const askChad = async(userInput) => {
     try {
-      const response = await fetch()
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: 'gpt-3.5-turbo',
+          messages: [{role: 'system', content:
+        "You are a wacky but hopeful future inventor of sustainable inventions"}, {role: 'user', content: `${userInput}`}]
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      setResult(data.choices[0].message.content)
+      console.log(result)
+      toggleForm();
+    } catch(error) {
+      console.log(error);
     }
   }
 
   let currentlyVisible = null;
   if (formVisible) {
-    currentlyVisible = <Form talkToChad={askChad} />
+    currentlyVisible = <Form talkToChad={askChad}/>
   } else {
     currentlyVisible = <Result back={toggleForm} message={result} />
   }
