4c94cbeceb2dd3e23d67d6ab2ad1cf2a87fea690
Emma Gerigscott <elgerig@gmail.com>
2023-08-17 10:05:26 -0700

    import and return header + controller

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import ChadController from './ChadController'
+import Header from './Header'
 
 function App() {
 
   return (
     <>
+      <Header />
+      <ChadController />
     </>
   )
 }
